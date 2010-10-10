47cf9171630394efb92cf5b198829065472a9c3e
Jarmo Pertman <jarmo.p@gmail.com>
2010-10-10 22:04:16 +0300

    make extract private

--- lib/rautomation/autoit/locators.rb
+++ lib/rautomation/autoit/locators.rb
@@ -1,6 +1,9 @@
 module RAutomation
   module AutoIt
     module Locators
+
+      private
+
       def extract(locators) #:nodoc:
         @locators = "[#{locators.map do |locator, value|
           locator_key = self.class::LOCATORS[locator] || self.class::LOCATORS[[locator, value.class]]
