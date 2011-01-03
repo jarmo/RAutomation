4127c4cb3c0e0fb3f98e663ebcae3d9a34f738b7
Stephan Schwab <sns@caimito.net>
2011-01-03 12:31:55 -0500

    New version of sample app with more controls and logic

---
+++ ext/WindowsForms/src/WindowsForms/WindowsForms/DataEntryForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsForms
+{
+    public partial class DataEntryForm : Form
+    {
+        public DataEntryForm()
+        {
+            InitializeComponent();
+        }
+
+        private void closeDataEntryFormButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void addItemButton_Click(object sender, EventArgs e)
+        {
+            PersonForm personForm = new PersonForm(this);
+            personForm.Show();
+        }
+
+        public void addPerson(String personName, String dateOfBirth)
+        {
+            ListViewItem newItem = new ListViewItem(personName);
+            newItem.SubItems.Add(dateOfBirth);
+            personListView.Items.Add(newItem);
+        }
+
+        private void deleteItemButton_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem item in personListView.SelectedItems)
+            {
+                personListView.Items.Remove(item);
+            }
+        }
+    }
+}
---
+++ ext/WindowsForms/src/WindowsForms/WindowsForms/PersonForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsForms
+{
+    public partial class PersonForm : Form
+    {
+        private DataEntryForm dataEntryForm;
+
+        public PersonForm(DataEntryForm dataEntryForm)
+        {
+            InitializeComponent();
+            this.dataEntryForm = dataEntryForm;
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            dataEntryForm.addPerson(personName.Text, personDateOfBirth.Text);
+            this.Close();
+        }
+
+    }
+}
