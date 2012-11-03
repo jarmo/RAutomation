9ad48b506b77f044abfe6d3366ab6f30056bfad4
Levi Wilson <levi@leviwilson.com>
2012-11-03 00:57:33 -0400

    convert UiaDll project to MC++

---
+++ ext/UiaDll/UiaDll/UiaDll.h
@@ -0,0 +1,13 @@
+// UiaDll.h
+
+#pragma once
+
+using namespace System;
+
+namespace UiaDll {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
--- ext/UiaDll/UiaDll/dllmain.cpp
+++ ext/UiaDll/UiaDll/dllmain.cpp
@@ -4,36 +4,18 @@
 IUIAutomation *pAutomation ;
 
 IUIAutomation* getGlobalIUIAutomation() {
-	return pAutomation ;
-}
-
-
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-					 )
-{
-	switch (ul_reason_for_call)
-	{
-	case DLL_PROCESS_ATTACH: 
-	case DLL_THREAD_ATTACH:
+	if( NULL == pAutomation ) {
 		HRESULT hr ;
 		hr = CoInitialize(NULL) ;
 		if (FAILED(hr)) {
 			printf("UiaDll: CoInitialize failed. hr = 0x%x", hr) ;
-			return FALSE ;
+			return NULL;
 		}
 		hr = CoCreateInstance(__uuidof(CUIAutomation), NULL, CLSCTX_INPROC_SERVER, __uuidof(IUIAutomation), (void**)&pAutomation);
 		if (FAILED(hr)) {
 			printf("UiaDll: CoCreateInstance failed. hr = 0x%x", hr) ;
-			return FALSE ;
+			return NULL;
 		}
-		break ;
-	case DLL_THREAD_DETACH:
-	case DLL_PROCESS_DETACH:
-		CoUninitialize() ;
-		break;
 	}
-	return TRUE;
+	return pAutomation ;
 }
-
