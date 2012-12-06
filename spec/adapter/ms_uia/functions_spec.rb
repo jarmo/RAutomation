05dae752982b5e90fd207e11172a3a7b95accd87
Levi Wilson <levi@leviwilson.com>
2012-12-06 11:06:50 -0500

    reorg functions specs

--- spec/adapter/ms_uia/functions_spec.rb
+++ spec/adapter/ms_uia/functions_spec.rb
@@ -7,17 +7,19 @@ describe "MsUia::Functions", :if => SpecHelper.adapter == :ms_uia do
     tree_view.options.map &:text
   end
 
-  it "can expand items by value" do
-    options.should eq ["Parent One", "Parent Two"]
-    tree_view.expand "Parent One"
-    options.should eq ["Parent One", "Child 1", "Child 2", "Parent Two"]
-  end
+  context "working with expandable / collapsable items" do
+    it "can be expanded by value" do
+      options.should eq ["Parent One", "Parent Two"]
+      tree_view.expand "Parent One"
+      options.should eq ["Parent One", "Child 1", "Child 2", "Parent Two"]
+    end
 
-  it "can expand items by index" do
-    options.should eq ["Parent One", "Parent Two"]
-    tree_view.expand 0
-    tree_view.expand 2
-    options.should eq ["Parent One", "Child 1", "Child 2", "Grandchild 1", "Parent Two"]
+    it "can be expanded by index" do
+      options.should eq ["Parent One", "Parent Two"]
+      tree_view.expand 0
+      tree_view.expand 2
+      options.should eq ["Parent One", "Child 1", "Child 2", "Grandchild 1", "Parent Two"]
+    end
   end
 
 end
