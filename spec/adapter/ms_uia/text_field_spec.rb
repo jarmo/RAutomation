560b5affc6463f65b5e62e2dac38618b4d99cc5a
Levi Wilson <levi@leviwilson.com>
2013-01-17 11:29:32 -0500

    DRY up text_field_spec usage of the main form

--- spec/adapter/ms_uia/text_field_spec.rb
+++ spec/adapter/ms_uia/text_field_spec.rb
@@ -1,31 +1,31 @@
 require 'spec_helper'
 
 describe "MsUia::TextField", :if => SpecHelper.adapter == :ms_uia do
+  let(:main_form) { RAutomation::Window.new(:title => "MainFormWindow") }
 
   it "check for text field class" do
-    RAutomation::Window.new(:title => "MainFormWindow").text_field(:id => "checkBox").should_not exist
-    RAutomation::Window.new(:title => "MainFormWindow").text_field(:id => "textField").should exist
+    main_form.text_field(:id => "checkBox").should_not exist
+    main_form.text_field(:id => "textField").should exist
   end
 
   it "enabled/disabled" do
-    RAutomation::Window.new(:title => "MainFormWindow").text_field(:id => "textField").should be_enabled
-    RAutomation::Window.new(:title => "MainFormWindow").text_field(:id => "textField").should_not be_disabled
+    main_form.text_field(:id => "textField").should be_enabled
+    main_form.text_field(:id => "textField").should_not be_disabled
 
-    RAutomation::Window.new(:title => "MainFormWindow").text_field(:id => "textBoxDisabled").should_not be_enabled
-    RAutomation::Window.new(:title => "MainFormWindow").text_field(:id => "textBoxDisabled").should be_disabled
+    main_form.text_field(:id => "textBoxDisabled").should_not be_enabled
+    main_form.text_field(:id => "textBoxDisabled").should be_disabled
   end
 
   it "cannot set a value to a disabled text field" do
-    lambda { RAutomation::Window.new(:title => "MainFormWindow").text_field(:id => "textBoxDisabled").set "abc" }.should raise_error
+    lambda { main_form.text_field(:id => "textBoxDisabled").set "abc" }.should raise_error
 
-    lambda { RAutomation::Window.new(:title => "MainFormWindow").text_field(:id => "textBoxDisabled").clear }.should raise_error
+    lambda { main_form.text_field(:id => "textBoxDisabled").clear }.should raise_error
   end
 
   it "considers a document control type a text field as well", :focus => true do
     # cause the .NET framework to be loaded into the process (required to make this fail)
-    RAutomation::Window.new(:title => "MainFormWindow").select_list(:id => "treeView").expand 0
-
-    RAutomation::Window.new(:title => "MainFormWindow").text_field(:id => "multiLineTextField").should exist
+    main_form.select_list(:id => "treeView").expand 0
+    main_form.text_field(:id => "multiLineTextField").should exist
   end
 
 end
