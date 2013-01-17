75b027d5863d73260b7f3f88c81e98ddd2c646f3
Levi Wilson <levi@leviwilson.com>
2013-01-17 11:00:56 -0500

    DRY up text_field_spec

--- spec/text_field_spec.rb
+++ spec/text_field_spec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
 
 describe RAutomation::TextField do
+  let(:main_form) { RAutomation::Window.new(:title => "MainFormWindow") }
+  let(:text_field) { main_form.text_field(:class => /Edit/i, :index => 2) }
+
   it "#text_field" do
-    RAutomation::Window.new(:title => "MainFormWindow").
-        text_field(:class => /Edit/i, :index => 1).should exist
+    main_form.text_field(:class => /Edit/i, :index => 1).should exist
 
     RAutomation::Window.wait_timeout = 0.1
     expect { RAutomation::Window.new(:title => "non-existent-window").
@@ -12,53 +14,46 @@ describe RAutomation::TextField do
   end
 
   it "#set" do
-    window = RAutomation::Window.new(:title => "MainFormWindow")
-    window.text_field(:class => /Edit/i, :index => 2).set "hello!"
+    text_field.set "hello!"
 
     RAutomation::Window.wait_timeout = 0.1
-    expect { window.text_field(:class => "non-existing-field").set "hello!" }.
+    expect { main_form.text_field(:class => "non-existing-field").set "hello!" }.
         to raise_exception(RAutomation::UnknownTextFieldException)
   end
 
   it "#clear" do
-    window = RAutomation::Window.new(:title => "MainFormWindow")
-    field  = window.text_field(:class => /Edit/i, :index => 2)
-    field.set "hello!"
-    field.value.should == "hello!"
-    field.clear
-    field.value.should be_empty
+    text_field.set "hello!"
+    text_field.value.should == "hello!"
+    text_field.clear
+    text_field.value.should be_empty
 
     RAutomation::Window.wait_timeout = 0.1
-    expect { window.text_field(:class => "non-existent-field").clear }.
+    expect { main_form.text_field(:class => "non-existent-field").clear }.
         to raise_exception(RAutomation::UnknownTextFieldException)
   end
 
   it "#value" do
-    window = RAutomation::Window.new(:title => "MainFormWindow")
-    field  = window.text_field(:class => /Edit/i, :index => 2)
-    field.set "hello!"
-    field.value.should == "hello!"
+    text_field.set "hello!"
+    text_field.value.should == "hello!"
 
     RAutomation::Window.wait_timeout = 0.1
-    expect { window.text_field(:class => "non-existent-field").value }.
+    expect { main_form.text_field(:class => "non-existent-field").value }.
         to raise_exception(RAutomation::UnknownTextFieldException)
   end
 
   it "#exists?" do
-    window = RAutomation::Window.new(:title => "MainFormWindow")
-    field  = window.text_field(:class => /Edit/i, :index => 1)
+    field  = main_form.text_field(:class => /Edit/i, :index => 1)
     field.should exist
-    window.text_field(:class => "non-existent-field").should_not exist
+    main_form.text_field(:class => "non-existent-field").should_not exist
   end
 
 
   it "#hwnd" do
-    window = RAutomation::Window.new(:title => "MainFormWindow")
-    field  = window.text_field(:class => /Edit/i, :index => 1)
+    field  = main_form.text_field(:class => /Edit/i, :index => 1)
     field.hwnd.should be_a(Fixnum)
 
     RAutomation::Window.wait_timeout = 0.1
-    expect { window.text_field(:class => "non-existing-window").hwnd }.
+    expect { main_form.text_field(:class => "non-existing-window").hwnd }.
         to raise_exception(RAutomation::UnknownTextFieldException)
   end
 end
