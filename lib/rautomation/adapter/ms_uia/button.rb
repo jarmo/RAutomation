a01c3e711c218ee248c1fda7554544c67449a5ea
Levi Wilson <levi@leviwilson.com>
2013-04-06 23:40:20 -0400

    use SearchCriteria for Button#value

--- lib/rautomation/adapter/ms_uia/button.rb
+++ lib/rautomation/adapter/ms_uia/button.rb
@@ -8,17 +8,12 @@ module RAutomation
         # Default locators used for searching buttons.
         DEFAULT_LOCATORS = {:class => /button/i}
 
-        #todo - replace with UIA version
-        # @see RAutomation::Button#value
-        def value
-          Functions.control_value(Functions.control_hwnd(@window.hwnd, @locators))
-        end
-
         def exist?
           super && matches_type?(Constants::UIA_BUTTON_CONTROL_TYPE)
         end
 
         alias_method :exists?, :exist?
+        alias_method :value, :control_name
 
       end
     end
