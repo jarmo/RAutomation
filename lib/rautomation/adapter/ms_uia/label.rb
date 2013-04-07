20386bf29221c96551deff77fa2746f462bc3c69
Levi Wilson <levi@leviwilson.com>
2013-04-07 00:00:08 -0400

    use SearchCriteria for Label#value

--- lib/rautomation/adapter/ms_uia/label.rb
+++ lib/rautomation/adapter/ms_uia/label.rb
@@ -5,16 +5,12 @@ module RAutomation
         include WaitHelper
         include Locators
 
-        #todo - replace with UIA version
-        def value
-          Functions.control_value(Functions.control_hwnd(@window.hwnd, @locators))
-        end
-
         def exist?
           super && matches_type?(Constants::UIA_TEXT_CONTROL_TYPE)
         end
 
         alias_method :exists?, :exist?
+        alias_method :value, :control_name
 
       end
     end
