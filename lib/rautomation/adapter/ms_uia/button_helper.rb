3ef33ce57c4b659c69b00c6d14cee128932fd6fb
Levi Wilson <levi@leviwilson.com>
2013-04-22 17:21:15 -0400

    remove unused set? method from button_helper

--- lib/rautomation/adapter/ms_uia/button_helper.rb
+++ lib/rautomation/adapter/ms_uia/button_helper.rb
@@ -3,12 +3,6 @@ module RAutomation
     module MsUia
       module ButtonHelper
 
-        #todo - replace with UIA version
-        def set?
-          control_hwnd = Functions.control_hwnd(@window.hwnd, @locators)
-          Functions.control_set? control_hwnd
-        end
-
         # @todo call a windows function to do this without clicking
         def clear
           click {!set?} if set?
