ded73b50929a6d6104dcade551e3963d8ddc025f
Levi Wilson <levi@leviwilson.com>
2013-06-14 13:09:06 -0400

    AutomatedSelectList whitespace

--- ext/UiaDll/UiaDll/AutomatedSelectList.cpp
+++ ext/UiaDll/UiaDll/AutomatedSelectList.cpp
@@ -20,68 +20,68 @@ array<String^>^ AutomatedSelectList::Selection::get() {
 int AutomatedSelectList::GetOptions(const char* options[]) {
   auto selectionItems = SelectionItems;
 
-	if( NULL != options ) {
-		StringHelper::CopyNames(selectionItems, options);
-	}
+  if( NULL != options ) {
+    StringHelper::CopyNames(selectionItems, options);
+  }
 
-	return selectionItems->Count;
+  return selectionItems->Count;
 }
 
 bool AutomatedSelectList::SelectByIndex(const int whichItem)
 {
-	try {
-	  auto selectionItems = _control->FindAll(System::Windows::Automation::TreeScope::Subtree, SelectionCondition);
-	  Select(selectionItems[whichItem]);
-	  return true;
-	} catch(Exception^ e) {
-		Console::WriteLine(e->ToString());
-		return false;
-	}
+  try {
+    auto selectionItems = _control->FindAll(System::Windows::Automation::TreeScope::Subtree, SelectionCondition);
+    Select(selectionItems[whichItem]);
+    return true;
+  } catch(Exception^ e) {
+    Console::WriteLine(e->ToString());
+    return false;
+  }
 }
 
 bool AutomatedSelectList::SelectByValue(const char* whichItem)
 {
-	try {
-	  auto nameCondition = gcnew PropertyCondition(AutomationElement::NameProperty, gcnew String(whichItem));
-	  auto selectionAndNameCondition = gcnew AndCondition(SelectionCondition, nameCondition);
+  try {
+    auto nameCondition = gcnew PropertyCondition(AutomationElement::NameProperty, gcnew String(whichItem));
+    auto selectionAndNameCondition = gcnew AndCondition(SelectionCondition, nameCondition);
 
-	  Select(_control->FindFirst(System::Windows::Automation::TreeScope::Subtree, selectionAndNameCondition));
-	  return true;
-	} catch(Exception^ e) {
-		Console::WriteLine(e->ToString());
-		return false;
-	}
+    Select(_control->FindFirst(System::Windows::Automation::TreeScope::Subtree, selectionAndNameCondition));
+    return true;
+  } catch(Exception^ e) {
+    Console::WriteLine(e->ToString());
+    return false;
+  }
 }
 
 bool AutomatedSelectList::GetValueByIndex(const int whichItem, char* comboValue, const int comboValueSize)
 {
-	try {
-		auto selectionItem = SelectionItems[whichItem];
-		auto nameProperty = dynamic_cast<String^>(selectionItem->GetCurrentPropertyValue(AutomationElement::NameProperty));
+  try {
+    auto selectionItem = SelectionItems[whichItem];
+    auto nameProperty = dynamic_cast<String^>(selectionItem->GetCurrentPropertyValue(AutomationElement::NameProperty));
 
-		StringHelper::CopyToUnmanagedString(nameProperty, comboValue, comboValueSize);
-		return true;
-	} catch(Exception^ e) {
-		Console::WriteLine(e->ToString());
-		return false;
-	}
+    StringHelper::CopyToUnmanagedString(nameProperty, comboValue, comboValueSize);
+    return true;
+  } catch(Exception^ e) {
+    Console::WriteLine(e->ToString());
+    return false;
+  }
 }
 
 int AutomatedSelectList::SelectedIndex::get() {
-	int selectedIndex = 0;
-	for each(AutomationElement^ selectionItem in SelectionItems) {
-	  auto selectionPattern = dynamic_cast<SelectionItemPattern^>(selectionItem->GetCurrentPattern(SelectionItemPattern::Pattern));
-	  if( selectionPattern->Current.IsSelected ) {
-		  return selectedIndex;
-	  }
-	  ++selectedIndex;
-	}
-	return -1;
+  int selectedIndex = 0;
+  for each(AutomationElement^ selectionItem in SelectionItems) {
+    auto selectionPattern = dynamic_cast<SelectionItemPattern^>(selectionItem->GetCurrentPattern(SelectionItemPattern::Pattern));
+    if( selectionPattern->Current.IsSelected ) {
+      return selectedIndex;
+    }
+    ++selectedIndex;
+  }
+  return -1;
 }
 
 
 void AutomatedSelectList::Select(AutomationElement^ itemToSelect)
 {
-	auto selectionPattern = dynamic_cast<SelectionItemPattern^>(itemToSelect->GetCurrentPattern(SelectionItemPattern::Pattern));
-	selectionPattern->Select();
+  auto selectionPattern = dynamic_cast<SelectionItemPattern^>(itemToSelect->GetCurrentPattern(SelectionItemPattern::Pattern));
+  selectionPattern->Select();
 }
