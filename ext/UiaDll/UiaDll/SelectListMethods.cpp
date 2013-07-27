3aecd34387e96147b771bd8283fdf22dfdf67509
Levi Wilson <levi@leviwilson.com>
2013-07-27 02:16:18 -0400

    whitespace

--- ext/UiaDll/UiaDll/SelectListMethods.cpp
+++ ext/UiaDll/UiaDll/SelectListMethods.cpp
@@ -11,22 +11,22 @@ extern "C" {
 		return autoSelectList->Count;
 	}
 
-  __declspec ( dllexport ) int SelectList_Options(const FindInformation& findInformation, const char* options[]) {
-    auto selectList = gcnew SelectList(AutomationFinder::FindFor(findInformation));
-    return StringHelper::Copy(selectList->Options, options);
-  }
+	__declspec ( dllexport ) int SelectList_Options(const FindInformation& findInformation, const char* options[]) {
+		auto selectList = gcnew SelectList(AutomationFinder::FindFor(findInformation));
+		return StringHelper::Copy(selectList->Options, options);
+	}
 
 	__declspec ( dllexport ) int SelectList_SelectedIndex(const FindInformation& findInformation) {
-    auto selectList = gcnew SelectList(AutomationFinder::FindFor(findInformation));
+		auto selectList = gcnew SelectList(AutomationFinder::FindFor(findInformation));
 		return selectList->SelectedIndex;
 	}
 
-  __declspec ( dllexport ) void SelectList_Selection(const FindInformation& findInformation, char* selection, const int selectionLength) {
-    auto selectList = gcnew AutomatedSelectList(findInformation);
-    auto currentSelections = selectList->Selection;
-    auto firstSelection = currentSelections.Length == 0 ? "" : currentSelections[0];
-    StringHelper::CopyToUnmanagedString(firstSelection, selection, selectionLength);
-  }
+	__declspec ( dllexport ) void SelectList_Selection(const FindInformation& findInformation, char* selection, const int selectionLength) {
+		auto selectList = gcnew AutomatedSelectList(findInformation);
+		auto currentSelections = selectList->Selection;
+		auto firstSelection = currentSelections.Length == 0 ? "" : currentSelections[0];
+		StringHelper::CopyToUnmanagedString(firstSelection, selection, selectionLength);
+	}
 
 	__declspec ( dllexport ) bool SelectList_ValueAt(const FindInformation& findInformation, const int whichItem, char* comboValue, const int comboValueSize) {
 		auto autoSelectList = gcnew AutomatedSelectList(findInformation);
