5171f4bec7d18031a343cebbbfd4061031720732
Levi Wilson <levi@leviwilson.com>
2013-08-09 17:44:08 -0400

    add spinner methods to get and set the value

---
+++ ext/UiaDll/RAutomation.UIA/Controls/Spinner.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Automation;
+using RAutomation.UIA.Extensions;
+
+namespace RAutomation.UIA.Controls
+{
+    public class Spinner
+    {
+        private readonly AutomationElement _element;
+
+        public Spinner(AutomationElement element)
+        {
+            _element = element;
+        }
+
+        public double Value
+        {
+            get { return RangeValue.Current.Value; }
+            set { RangeValue.SetValue(value); }
+        }
+
+        private RangeValuePattern RangeValue
+        {
+            get { return _element.As<RangeValuePattern>(RangeValuePattern.Pattern); }
+        }
+    }
+}
---
+++ ext/UiaDll/UiaDll/SpinnerMethods.cpp
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+#include "Locator.h"
+#include "StringHelper.h"
+
+using namespace RAutomation::UIA::Controls;
+
+extern "C" {
+	__declspec (dllexport) double Spinner_GetValue(const FindInformation& findInformation, char* errorInfo, const int errorInfoLength) {
+		try {
+			auto spinner = gcnew Spinner(Locator::FindFor(findInformation));
+      return spinner->Value;
+		} catch(Exception^ e) {
+			StringHelper::CopyToUnmanagedString(e->Message, errorInfo, errorInfoLength);
+		}
+	}
+
+	__declspec (dllexport) void Spinner_SetValue(const FindInformation& findInformation, double theValue, char* errorInfo, const int errorInfoLength) {
+		try {
+			auto spinner = gcnew Spinner(Locator::FindFor(findInformation));
+      spinner->Value = theValue;
+		} catch(Exception^ e) {
+			StringHelper::CopyToUnmanagedString(e->Message, errorInfo, errorInfoLength);
+		}
+	}
+}
