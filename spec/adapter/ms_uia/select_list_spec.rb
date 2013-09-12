e4e139c18ffd6c76cff00fa11c782fa52ef7cd1a
Levi Wilson <levi@leviwilson.com>
2013-09-11 23:14:22 -0400

    cleaned up select_list_spec

--- spec/adapter/ms_uia/select_list_spec.rb
+++ spec/adapter/ms_uia/select_list_spec.rb
@@ -1,68 +1,66 @@
 require 'spec_helper'
 
-describe "MsUia::SelectList", :if => SpecHelper.adapter == :ms_uia do
+describe 'MsUia::SelectList', :if => SpecHelper.adapter == :ms_uia do
   let(:window) { RAutomation::Window.new(:title => 'MainFormWindow') }
+  let(:fruits_combo) { window.select_list(:id => 'FruitsComboBox') }
   let(:fruits_list) { window.select_list(:id => 'FruitListBox') }
   let(:fruit_label) { window.label(:id => 'fruitsLabel') }
+  let(:disabled_combo) { window.select_list(:id => 'comboBoxDisabled') }
 
-  it "#select_list" do
-    RAutomation::Window.new(:title => "MainFormWindow").select_list(:id => "FruitsComboBox").should exist
+  it '#fruits_combo' do
+    fruits_combo.should exist
 
     RAutomation::Window.wait_timeout = 0.1
-    expect {RAutomation::Window.new(:title => "non-existent-window").
+    expect {RAutomation::Window.new(:title => 'non-existent-window').
             select_list(:class => /COMBOBOX/i)}.
             to raise_exception(RAutomation::UnknownWindowException)
   end
 
-  it "check for select list class" do
-    RAutomation::Window.new(:title => "MainFormWindow").select_list(:id => "textField").should_not exist
-    RAutomation::Window.new(:title => "MainFormWindow").select_list(:id => "FruitsComboBox").should exist
+  it 'check for select list class' do
+    window.select_list(:id => 'textField').should_not exist
+    fruits_combo.should exist
   end
 
-  it "#options" do
-    select_list = RAutomation::Window.new(:title => "MainFormWindow").select_list(:id => "FruitsComboBox")
-    select_list.options.size.should == 5
+  it '#options' do
+    fruits_combo.options.size.should == 5
 
-    expected_options = ["Apple", "Caimito", "Coconut", "Orange", "Passion Fruit"]
-    select_list.options.map {|option| option.text}.should == expected_options
+    expected_options = ['Apple', 'Caimito', 'Coconut', 'Orange', 'Passion Fruit']
+    fruits_combo.options.map {|option| option.text}.should == expected_options
   end
 
-  it "#selected? & #select" do
-    select_list = RAutomation::Window.new(:title => "MainFormWindow").select_list(:id => "FruitsComboBox")
-    select_list.options(:text => "Apple")[0].should_not be_selected
-    select_list.options(:text => "Apple")[0].select.should be_true
-    select_list.options(:text => "Apple")[0].should be_selected
+  it '#selected? & #select' do
+    fruits_combo.options(:text => 'Apple')[0].should_not be_selected
+    fruits_combo.options(:text => 'Apple')[0].select.should be_true
+    fruits_combo.options(:text => 'Apple')[0].should be_selected
   end
 
-  it "#set" do
-    select_list = RAutomation::Window.new(:title => "MainFormWindow").select_list(:id => "FruitsComboBox")
-    select_list.options(:text => "Apple")[0].should_not be_selected
-    select_list.set("Apple")
-    select_list.options(:text => "Apple")[0].should be_selected
+  it '#set' do
+    fruits_combo.options(:text => 'Apple')[0].should_not be_selected
+    fruits_combo.set('Apple')
+    fruits_combo.options(:text => 'Apple')[0].should be_selected
   end
 
-  it "#value" do
-    select_list = RAutomation::Window.new(:title => "MainFormWindow").select_list(:id => "FruitsComboBox")
+  it '#value' do
 
     #default empty state
-    select_list.value.should == ""
+    fruits_combo.value.should == ''
 
-    select_list.options(:text => "Apple")[0].select
-    select_list.value.should == "Apple"
+    fruits_combo.options(:text => 'Apple')[0].select
+    fruits_combo.value.should == 'Apple'
 
-    select_list.options(:text => "Caimito")[0].select
-    select_list.value.should == "Caimito"
+    fruits_combo.options(:text => 'Caimito')[0].select
+    fruits_combo.value.should == 'Caimito'
   end
 
-  it "enabled/disabled" do
-    RAutomation::Window.new(:title => "MainFormWindow").select_list(:id => "FruitsComboBox").should be_enabled
-    RAutomation::Window.new(:title => "MainFormWindow").select_list(:id => "FruitsComboBox").should_not be_disabled
+  it 'enabled/disabled' do
+    fruits_combo.should be_enabled
+    fruits_combo.should_not be_disabled
 
-    RAutomation::Window.new(:title => "MainFormWindow").select_list(:id => "comboBoxDisabled").should_not be_enabled
-    RAutomation::Window.new(:title => "MainFormWindow").select_list(:id => "comboBoxDisabled").should be_disabled
+    disabled_combo.should_not be_enabled
+    disabled_combo.should be_disabled
   end
 
-  it "#add" do
+  it '#add' do
     fruits_list.add(0, 1)
     fruit_label.value.should eq('Apple,Orange')
 
@@ -70,7 +68,7 @@ describe "MsUia::SelectList", :if => SpecHelper.adapter == :ms_uia do
     fruit_label.value.should eq('Apple,Orange,Mango')
   end
 
-  it "#remove" do
+  it '#remove' do
     fruits_list.add('Apple', 'Orange', 'Mango')
 
     fruits_list.remove('Orange')
@@ -80,49 +78,37 @@ describe "MsUia::SelectList", :if => SpecHelper.adapter == :ms_uia do
     fruits_list.values.should eq(['Mango'])
   end
 
-  it "#values" do
+  it '#values' do
     fruits_list.add('Apple', 'Mango')
     fruits_list.values.should eq(['Apple', 'Mango'])
   end
 
-  it "#option" do
-    select_list = RAutomation::Window.new(:title => "MainFormWindow").select_list(:id => "FruitsComboBox")
-
-    select_list.option(:text => "Apple").should_not be_selected
-    select_list.option(:text => "Apple").set
-    select_list.option(:text => "Apple").should be_selected
+  it '#option' do
+    fruits_combo.option(:text => 'Apple').should_not be_selected
+    fruits_combo.option(:text => 'Apple').set
+    fruits_combo.option(:text => 'Apple').should be_selected
   end
 
-  it "cannot select anything on a disabled select list" do
-    select_list = RAutomation::Window.new(:title => "MainFormWindow").select_list(:id => "comboBoxDisabled")
-
-    lambda { select_list.option(:text => "Apple").set }.should raise_error
+  it 'cannot select anything on a disabled select list' do
+    lambda { disabled_combo.option(:text => 'Apple').set }.should raise_error
   end
 
-  it "fires change event when the value is set" do
+  it 'fires change event when the value is set' do
+    fruits_combo.option(:text => 'Apple').should_not be_selected
+    fruits_combo.set('Apple')
+    fruits_combo.option(:text => 'Apple').should be_selected
 
-    select_list = RAutomation::Window.new(:title => "MainFormWindow").select_list(:id => "FruitsComboBox")
-
-    select_list.option(:text => "Apple").should_not be_selected
-    select_list.set("Apple")
-    select_list.option(:text => "Apple").should be_selected
-
-    label = RAutomation::Window.new(:title => "MainFormWindow").label(:id => "fruitsLabel")
-    RAutomation::WaitHelper.wait_until { label.exist? }
-    label.value.should == "Apple"
+    RAutomation::WaitHelper.wait_until { fruit_label.exist? }
+    fruit_label.value.should == 'Apple'
   end
 
-  it "fires change event when the index changes" do
+  it 'fires change event when the index changes' do
+    fruits_combo.options[4].select
+    fruits_combo.option(:text => 'Passion Fruit').should be_selected
+    fruit_label.value.should == 'Passion Fruit'
 
-    select_list = RAutomation::Window.new(:title => "MainFormWindow").select_list(:id => "FruitsComboBox")
-    label = RAutomation::Window.new(:title => "MainFormWindow").label(:id => "fruitsLabel")
-
-    select_list.options[4].select
-    select_list.option(:text => "Passion Fruit").should be_selected
-    label.value.should == "Passion Fruit"
-
-    select_list.select 3
-    select_list.option(:text => "Orange").should be_selected
-    label.value.should == "Orange"
+    fruits_combo.select 3
+    fruits_combo.option(:text => 'Orange').should be_selected
+    fruit_label.value.should == 'Orange'
   end
 end
