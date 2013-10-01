13a5cf5196c68921e5f3a3299289f058730bcef3
Levi Wilson <levi@leviwilson.com>
2013-10-01 18:40:23 -0400

    refactor ListBox specs

--- spec/adapter/ms_uia/listbox_spec.rb
+++ spec/adapter/ms_uia/listbox_spec.rb
@@ -2,32 +2,29 @@ require 'spec_helper'
 
 describe "MsUia::ListBox", :if => SpecHelper.adapter == :ms_uia do
   let(:window) { RAutomation::Window.new(:title => "MainFormWindow") }
+  let(:label) { window.label(:id => "fruitsLabel") }
+  let(:list_box) { window.list_box(:id => "FruitListBox") }
 
   it "#exists" do
-    RAutomation::Window.new(:title => "MainFormWindow").list_box(:id => "FruitListBox").should exist
+    list_box.should exist
   end
 
   it "checks for ListBox class" do
-    RAutomation::Window.new(:title => "MainFormWindow").list_box(:id => "textField").should_not exist
-    RAutomation::Window.new(:title => "MainFormWindow").list_box(:id => "FruitListBox").should exist
+    window.list_box(:id => "textField").should_not exist
+    list_box.should exist
   end
 
   it "counts items" do
-    list_box = RAutomation::Window.new(:title => "MainFormWindow").list_box(:id => "FruitListBox")
     list_box.count.should == 3
   end
 
   it "lists items" do
-    list_box = RAutomation::Window.new(:title => "MainFormWindow").list_box(:id => "FruitListBox")
-
     list_box.items[0].value.should == "Apple"
     list_box.items[1].value.should == "Orange"
     list_box.items[2].value.should == "Mango"
   end
 
   it "returns a value" do
-    list_box = RAutomation::Window.new(:title => "MainFormWindow").list_box(:id => "FruitListBox")
-
     list_box.value.should == ""
     list_box.select(0)
     list_box.value.should == "Apple"
@@ -38,16 +35,12 @@ describe "MsUia::ListBox", :if => SpecHelper.adapter == :ms_uia do
   end
 
   it "#selected?" do
-    list_box = RAutomation::Window.new(:title => "MainFormWindow").list_box(:id => "FruitListBox")
-
     list_box.selected?(2).should == false
     list_box.select(2)
     list_box.selected?(2).should == true
   end
 
   it "#select" do
-    list_box = RAutomation::Window.new(:title => "MainFormWindow").list_box(:id => "FruitListBox")
-
     list_box.select(1)
     list_box.selected?(1).should == true
 
@@ -59,15 +52,10 @@ describe "MsUia::ListBox", :if => SpecHelper.adapter == :ms_uia do
   end
 
   it "#strings" do
-    list_box = RAutomation::Window.new(:title => "MainFormWindow").list_box(:id => "FruitListBox")
-
     list_box.strings.should == ["Apple", "Orange", "Mango"]
   end
 
   it "fires events when the index changes" do
-    list_box = RAutomation::Window.new(:title => "MainFormWindow").list_box(:id => "FruitListBox")
-    label = RAutomation::Window.new(:title => "MainFormWindow").label(:id => "fruitsLabel")
-
     ['Apple', 'Orange', 'Mango'].each_with_index do |value, index|
       list_box.select index
       label.value.should eq(value)
@@ -76,8 +64,6 @@ describe "MsUia::ListBox", :if => SpecHelper.adapter == :ms_uia do
 
   it 'fires events even when the item is not in the current view' do
     window.menu(text: 'File').menu(text: 'Add Some Fruits').open
-    list_box = window.list_box(:id => "FruitListBox")
-    label = window.label(:id => "fruitsLabel")
 
     list_box.strings.each_with_index do |value, index|
       list_box.select index
